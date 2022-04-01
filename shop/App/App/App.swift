ea77b5614041631f2fb4d500e666b3424a328a64
yangbing <419425903@qq.com>
2022-04-01 22:16:37 +0800

    111

--- shop/App/App/App.swift
+++ shop/App/App/App.swift
@@ -6,7 +6,21 @@
 //
 
 import UIKit
+import SDWebImage
 
 public class App: NSObject {
+   
+    lazy var image:UIImageView = {
+        let image = UIImageView()
+        return image
+    }()
+    override init(){
+        
+
+//        image.sd_setImage(with: <#T##URL?#>, completed: <#T##SDExternalCompletionBlock?##SDExternalCompletionBlock?##(UIImage?, Error?, SDImageCacheType, URL?) -> Void#>)
+        
+        
+    }
+    
 
 }
---
+++ shop/shop-bridge-header.h
@@ -0,0 +1,14 @@
+//
+//  shop-bridge-header.h
+//  shop
+//
+//  Created by mac on 2022/4/1.
+//
+
+#ifndef shop_bridge_header_h
+#define shop_bridge_header_h
+
+
+
+
+#endif /* shop_bridge_header_h */
